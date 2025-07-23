cb0812c7cce0d706caac4f7f83d8ae07162dcb13
Brenelle <22a28.brenelle@sjec.ac.in>
2025-07-23 20:02:44 +0530

    bug:Update api for security

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -38,9 +38,7 @@ const Weather = () => {
             return;
         }
         try{
-            const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
-
-            const response=await fetch(url);
+            const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`);
             const data=await response.json();
 
             if(!response.ok){
